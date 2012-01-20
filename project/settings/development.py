19c24868f246c3b061a75e3e0ce3467614dcdd90
Chris Gemignani <chris.gemignani@juiceanalytics.com>
2012-01-20 13:04:45 -0500

    Make Django debug toolbar optional

--- project/settings/development.py
+++ project/settings/development.py
@@ -3,22 +3,15 @@ from .production import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 DJANGO_SERVE_PUBLIC = True
+USE_DJANGO_DEBUG_TOOLBAR = True
 PREPEND_WWW = False
 SEND_BROKEN_LINK_EMAILS = False
 SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 
-# APP: debug_toolbar
-MIDDLEWARE_CLASSES += (
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-)
-
-INSTALLED_APPS += (
-    "debug_toolbar",
-)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
+if USE_DJANGO_DEBUG_TOOLBAR:
+    MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
+    INSTALLED_APPS += ("debug_toolbar",)
+    DEBUG_TOOLBAR_CONFIG = { 'INTERCEPT_REDIRECTS': False, }
 
 TEMPLATE_CONTEXT_PROCESSORS += [
     'django.core.context_processors.debug'
